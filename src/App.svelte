242604f55e6950748bf6ec767f900d76874714a1
Priya Ranjan Singh <mail@priyaranjan.net>
2023-02-11 12:22:54 +0530

    add Places component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import Places from './lib/Places.svelte'
 </script>
 
 <main>
@@ -16,6 +17,10 @@
 
   <div class="card">
     <Counter />
+  </div>
+
+  <div>
+    <Places />
   </div>
 
   <p>
---
+++ src/lib/Places.svelte
@@ -0,0 +1,45 @@
+<script>
+  let places = [
+    'Goa, India',
+    'Bali, Indonesia',
+    'Ho Chi Minh, Vietnam',
+  ];
+  let place = undefined;
+  const handleClick = () => {
+    places = places.concat(place);
+    place = undefined;
+  }
+  const handleClickRemove = (place) => {
+    const filteredPlaces = places.filter(i => i !== place);
+    places = filteredPlaces;
+  }
+</script>
+
+<div>
+  <ul>
+    {#each places as place}
+      <li>
+        {place}
+        <button on:click={() => handleClickRemove(place)} class=unstyled>&#10006;</button>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<div class="places">
+  <div><label for=place>Enter a new place</label></div>
+  <input name=place id=place bind:value={place} />
+  <button on:click={handleClick} disabled={place === undefined}>Add</button>
+</div>
+
+<style>
+  li, .places {
+    text-align: left;
+  }
+  input {
+    padding: 0.5em;
+  }
+  button.unstyled {
+    padding: 0.2em 0.3em;
+  }
+</style>
